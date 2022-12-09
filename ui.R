bb506dc91b537211255b03a362304ed8f4d5ae7e
Brian Jungmin Park <76887483+snowoflondon@users.noreply.github.com>
2022-12-09 14:40:50 -0500

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,51 @@
+library(shinythemes)
+library(DT)
+
+responseChoices <- c('Viability', 'Inhibition')
+
+fluidPage(
+  theme = shinytheme('flatly'),
+  titlePanel('CIcomputeR'),
+  fluidRow(
+    column(width = 4,
+           verticalLayout(
+             p('Upload your file here!'),
+             fileInput(inputId = 'fileSelect',
+                       accept = c('text/csv', 'text/comma-separated-values', '.csv'),
+                       label = 'Open'),
+             selectInput(inputId = 'responseSelect',
+                         label = 'Cell Response Metric: ',
+                         choices = as.list(responseChoices)),
+             checkboxInput(inputId = 'checkSelect',
+                           label = 'Response as Percentage? Check for Yes ',
+                           value = FALSE),
+             actionButton(inputId = 'buttonSelect',
+                          label = 'Run CIComputeR'),
+             actionButton(inputId = 'helpSelect',
+                          label = 'Help', 
+                          style='font-size:80%')
+           )),
+    column(width = 8,
+           DT::dataTableOutput(outputId = 'table'))
+  ),
+  fluidRow(
+    column(width = 12,
+           plotOutput(outputId = 'plot1'))
+  ),
+  fluidRow(
+    column(width = 12,
+           downloadButton(outputId = 'fileDownload',
+                          label = 'Download Result Table'))
+  ),
+  fluidRow(
+    column(width = 12,
+           downloadButton(outputId = 'exampleSelect',
+                          label = 'Download Example Data'))
+  ),
+  fluidRow(
+    column(width = 12,
+           a(href = 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4759401/',
+                'Reference'))
+  )
+)
+
